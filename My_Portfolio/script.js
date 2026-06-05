efb53bb0f14e961d5073ec7592d887e01a79fc71
vaishnavi9786 <vaishnavidhopade@gmail.com>
2026-06-05 14:57:06 +0530

    Portfolio

--- My_Portfolio/script.js
+++ My_Portfolio/script.js
@@ -1,21 +1,35 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", () => {
 
-    const form =
-    document.getElementById("contactForm");
+    const form = document.getElementById("contactForm");
 
-    form.addEventListener("submit", function(e){
+    form.addEventListener("submit", (e) => {
 
         e.preventDefault();
 
-        alert("✅ Message Sent Successfully!");
+        const popup = document.createElement("div");
+
+        popup.innerHTML = "✅ Your Message Sent Successfully!";
+
+        popup.style.position = "fixed";
+        popup.style.top = "20px";
+        popup.style.right = "20px";
+        popup.style.padding = "15px 25px";
+        popup.style.background = "#28a745";
+        popup.style.color = "#fff";
+        popup.style.borderRadius = "10px";
+        popup.style.fontSize = "16px";
+        popup.style.fontWeight = "bold";
+        popup.style.zIndex = "9999";
+        popup.style.boxShadow = "0 5px 15px rgba(0,0,0,0.2)";
+
+        document.body.appendChild(popup);
 
         form.reset();
 
-        setTimeout(function(){
-
-            location.reload();
-
-        },1000);
+        setTimeout(() => {
+            popup.remove();
+            location.reload(); // Page Refresh
+        }, 3000); // 3 seconds
 
     });
 
